d5f1d9fb0c26311107294b9c731b635af20dfa2d
ykroman <wsroman.rdky@gmail.com>
2020-06-22 23:36:32 +0900

    jenkinsのjunit test4

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,13 @@
+import org.junit.test;
+
 public class HelloWorld {
-    public static void main(String[] args){
-	int num = 5;
-        if(num == 5){
-		System.out.println("numは5です");
-	}else{
-		System.out.println("numは5ではありません");
-	}
-    }
+	@test
+	public static void main(String[] args){
+		int num = 5;
+		if(num == 5){
+			System.out.println("numは5です");
+		}else{
+			System.out.println("numは5ではありません");
+		}
+    	}
 }
